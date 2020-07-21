79878c40b32418466f775329042e09f882c19b60
Ketho <sghs17@outlook.com>
2020-07-21 06:26:13 +0200

    LoadOnDemand beta

--- KethoDoc/loadondemand.lua
+++ KethoDoc/loadondemand.lua
@@ -11,7 +11,7 @@ function KethoDoc:LoadLodAddons()
 	end
 end
 
--- 8.3.0.33051
+-- 8.3.0 (33051)
 KethoDoc.LoadOnDemand.live = {
 	"Blizzard_APIDocumentation",
 	"Blizzard_AchievementUI",
@@ -81,12 +81,93 @@ KethoDoc.LoadOnDemand.live = {
 	"Blizzard_WarfrontsPartyPoseUI",
 }
 
-KethoDoc.LoadOnDemand.ptr = {
-}
+KethoDoc.LoadOnDemand.ptr = KethoDoc.LoadOnDemand.live
 
-for _, v in pairs(KethoDoc.LoadOnDemand.live) do
-	tinsert(KethoDoc.LoadOnDemand.ptr, v)
-end
+-- 9.0.1 (35213)
+KethoDoc.LoadOnDemand.beta = {
+	"Blizzard_AchievementUI",
+	"Blizzard_AdventureMap",
+	"Blizzard_AlliedRacesUI",
+	"Blizzard_AnimaDiversionUI",
+	"Blizzard_APIDocumentation",
+	"Blizzard_ArchaeologyUI",
+	"Blizzard_ArenaUI",
+	"Blizzard_ArtifactUI",
+	"Blizzard_AuctionHouseUI",
+	"Blizzard_AzeriteEssenceUI",
+	"Blizzard_AzeriteRespecUI",
+	"Blizzard_AzeriteUI",
+	"Blizzard_BarberShopUI",
+	"Blizzard_BattlefieldMap",
+	"Blizzard_BindingUI",
+	"Blizzard_BlackMarketUI",
+	"Blizzard_BoostTutorial",
+	"Blizzard_Calendar",
+	"Blizzard_ChallengesUI",
+	"Blizzard_CharacterCreate",
+	"Blizzard_CharacterCustomize",
+	"Blizzard_ChromieTimeUI",
+	"Blizzard_ClassTrial",
+	"Blizzard_Collections",
+	"Blizzard_CombatLog",
+	"Blizzard_CombatText",
+	"Blizzard_Commentator",
+	"Blizzard_Communities",
+	"Blizzard_Contribution",
+	"Blizzard_CovenantCallings",
+	"Blizzard_CovenantPreviewUI",
+	"Blizzard_CovenantSanctum",
+	"Blizzard_DeathRecap",
+	"Blizzard_DebugTools",
+	"Blizzard_EncounterJournal",
+	"Blizzard_FlightMap",
+	"Blizzard_GarrisonTemplates",
+	"Blizzard_GarrisonUI",
+	"Blizzard_GMChatUI",
+	"Blizzard_GMSurveyUI",
+	"Blizzard_GuildBankUI",
+	"Blizzard_GuildControlUI",
+	"Blizzard_GuildRecruitmentUI",
+	"Blizzard_GuildUI",
+	"Blizzard_HybridMinimap",
+	"Blizzard_InspectUI",
+	"Blizzard_IslandsPartyPoseUI",
+	"Blizzard_IslandsQueueUI",
+	"Blizzard_ItemInteractionUI",
+	"Blizzard_ItemSocketingUI",
+	"Blizzard_ItemUpgradeUI",
+	"Blizzard_Kiosk",
+	"Blizzard_LandingSoulbinds",
+	"Blizzard_LookingForGuildUI",
+	"Blizzard_MacroUI",
+	"Blizzard_MapCanvas",
+	"Blizzard_MovePad",
+	"Blizzard_NewPlayerExperience",
+	"Blizzard_NewPlayerExperienceGuide",
+	"Blizzard_ObliterumUI",
+	"Blizzard_OrderHallUI",
+	"Blizzard_PartyPoseUI",
+	"Blizzard_PlayerChoiceUI",
+	"Blizzard_PTRFeedbackGlue", -- OnlyBetaAndPTR
+	"Blizzard_PVPUI",
+	"Blizzard_RaidUI",
+	"Blizzard_RuneforgeUI",
+	"Blizzard_ScrappingMachineUI",
+	"Blizzard_SharedMapDataProviders",
+	"Blizzard_SocialUI",
+	"Blizzard_Soulbinds",
+	"Blizzard_SubscriptionInterstitialUI",
+	"Blizzard_TalentUI",
+	"Blizzard_TalkingHeadUI",
+	"Blizzard_TimeManager",
+	"Blizzard_TradeSkillUI",
+	"Blizzard_TrainerUI",
+	"Blizzard_Tutorial",
+	"Blizzard_TutorialTemplates",
+	"Blizzard_VoidStorageUI",
+	"Blizzard_WarfrontsPartyPoseUI",
+	"Blizzard_WeeklyRewards",
+}
 
 -- 1.13.3 (32790)
 KethoDoc.LoadOnDemand.classic = {
