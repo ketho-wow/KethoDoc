03dbbc9968db5abc7357e86f73381ecb7ef87182
Daniel Yates <me@meorawr.io>
2021-04-13 22:13:17 +0100

    Add generation script

---
+++ Generate-Globals.ps1
@@ -0,0 +1,30 @@
+# $UiSourceUrl = "https://github.com/Gethe/wow-ui-source"
+$UiSourceUrl = "https://github.com/BigWigsMods/WoWUI"
+$UiSourceDir = "wow-ui-source"
+# $UiBranches  = @("live", "ptr", "classic", "classic_beta")
+$UiBranches  = @("ptr")
+
+if (-not (Test-Path $UiSourceDir)) {
+    & git clone $UiSourceUrl $UiSourceDir
+}
+
+foreach ($Branch in $UiBranches) {
+    Write-Output "Checking out UI source for $Branch..."
+    & git -C $UiSourceDir clean -fdx
+    & git -C $UiSourceDir checkout --force $Branch
+
+    Write-Output "Stripping UTF8 BOM from files..."
+    Get-ChildItem -Path $UiSourceDir -Recurse -Filter "*.lua" | ForEach-Object {
+        (Get-Content $_) | ForEach-Object { $_ -replace "\xEF\xBB\xBF", "" } | Set-Content $_
+    }
+
+    Write-Output "Generating globals..."
+    Get-ChildItem -Path $UiSourceDir -Recurse -Filter "*.lua" | ForEach-Object {
+        luac -l -p $_ | lua .\FindGlobals\globals.lua $_
+    } | Sort-Object -Unique | Out-File -Path ".\API_${Branch}.txt"
+
+    Write-Output "Generating LoD addons..."
+    Select-String -Path .\wow-ui-source\AddOns\*\*.toc -Pattern "## LoadOnDemand: 1" | ForEach-Object {
+        [IO.Path]::GetFileNameWithoutExtension($_.Filename)
+    } | Sort-Object | Out-File -Path ".\LoD_${Branch}.txt"
+}
