e2fef0201c5de7b5a3698b17b6a9ece16ee7b6f7
Ketho <sghs17@outlook.com>
2024-05-27 11:26:12 +0200

    refactoring

--- KethoDoc/KethoDoc.lua
+++ KethoDoc/KethoDoc.lua
@@ -60,9 +60,11 @@ end
 function KethoDoc:FixDocumentation()
 	-- 10.2.7
 	if Constants.PetConsts then
-		MAX_STABLE_SLOTS = Constants.PetConsts.MAX_STABLE_SLOTS
-		NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL = Constants.PetConsts.NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL
-		EXTRA_PET_STABLE_SLOT = Constants.PetConsts.EXTRA_PET_STABLE_SLOT
+		if not MAX_STABLE_SLOTS then
+			MAX_STABLE_SLOTS = Constants.PetConsts.MAX_STABLE_SLOTS
+			NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL = Constants.PetConsts.NUM_PET_SLOTS_THAT_NEED_LEARNED_SPELL
+			EXTRA_PET_STABLE_SLOT = Constants.PetConsts.EXTRA_PET_STABLE_SLOT
+		end
 	end
 end
 
